25c53968e8ce3ecfe70c53b028ba098f39faad0c
Vlad150288 <studiofocus.kr@gmail.com>
2026-03-25 11:59:26 +0200

    build pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-
-export default defineConfig({
-  site: 'https://vlad150288.github.io',
-  base: '/photostudio',
-  outDir: './docs',
-});
+export default defineConfig({});
